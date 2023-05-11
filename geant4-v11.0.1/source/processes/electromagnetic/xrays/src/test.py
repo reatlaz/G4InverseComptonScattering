d8082e41735c14e370c1679fb2e2009689eec66e
playatlas <atlasafri@gmail.com>
2023-05-12 02:26:27 +0300

    correct shape 27-30 keV plot in root

---
+++ geant4-v11.0.1/source/processes/electromagnetic/xrays/src/test.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import random
+N_BINS = 2000
+for n in range(N_BINS):
+    shoot = random.uniform(0.0, 10.0)
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
